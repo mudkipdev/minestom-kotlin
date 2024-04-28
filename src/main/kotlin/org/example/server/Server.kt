6aa0bf0f2c972201bf6341c46795b100c6a8774d
mudkipdev <mudkip@mudkip.dev>
2024-04-28 12:30:55 -0600

    switch to logback

--- src/main/kotlin/org/example/server/Server.kt
+++ src/main/kotlin/org/example/server/Server.kt
@@ -1,5 +1,9 @@
 package org.example.server
 
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.pattern.color.ANSIConstants
+import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase
 import net.minestom.server.MinecraftServer
 import net.minestom.server.coordinate.Pos
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
@@ -8,6 +12,17 @@ import net.minestom.server.instance.LightingChunk
 import net.minestom.server.instance.block.Block
 import net.minestom.server.utils.chunk.ChunkSupplier
 
+class LogbackHighlighter : ForegroundCompositeConverterBase<ILoggingEvent>() {
+    override fun getForegroundColorCode(event: ILoggingEvent): String {
+        return when (event.level.toInt()) {
+            Level.ERROR_INT -> ANSIConstants.BOLD + ANSIConstants.RED_FG
+            Level.WARN_INT -> ANSIConstants.BOLD + ANSIConstants.YELLOW_FG
+            Level.INFO_INT -> ANSIConstants.BLUE_FG
+            else -> ANSIConstants.DEFAULT_FG
+        }
+    }
+}
+
 fun main() {
     val server = MinecraftServer.init()
     val eventHandler = MinecraftServer.getGlobalEventHandler()
@@ -15,7 +30,7 @@ fun main() {
 
     // Creating a world
     val instance = instanceManager.createInstanceContainer()
-    instance.setChunkSupplier { _, x, z -> LightingChunk(instance, x, z) }
+    instance.chunkSupplier = ChunkSupplier(::LightingChunk)
     instance.setGenerator { it.modifier().fillHeight(-64, 0, Block.STONE) }
 
     instance.time = 6000
